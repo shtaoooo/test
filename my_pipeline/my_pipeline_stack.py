985383a29538051fbc31890fe18d0c02a4b7f30d
EC2 Default User <ec2-user@ip-172-31-29-18.ec2.internal>
2022-03-20 00:41:53 +0000

    initial commit

---
+++ my_pipeline/my_pipeline_stack.py
@@ -0,0 +1,18 @@
+import aws_cdk as cdk
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
+from constructs import Construct
+
+class MyPipelineStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        pipeline =  CodePipeline(self, "Pipeline", 
+                        pipeline_name="MyPipeline",
+                        synth=ShellStep("Synth", 
+                            input=CodePipelineSource.git_hub("shtaoooo/test", "main"),
+                            commands=["npm install -g aws-cdk", 
+                                "python -m pip install -r requirements.txt", 
+                                "cdk synth"]
+                        )
+                    )
